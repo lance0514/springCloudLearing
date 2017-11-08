698a0fee923c420de2bf2c02f2793a13daf851d2
xiaodong <pengwao@163.com>
2017-11-08 23:12:45 +0800

    first commit

---
+++ hello/src/main/java/com/didispace/hello/HelloController.java
@@ -0,0 +1,23 @@
+package com.didispace.hello;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+
+    @GetMapping("/hello")
+    public String index() {
+        ServiceInstance serviceInstance = client.getLocalServiceInstance();
+        logger.info("/hello,host:" + serviceInstance.getHost() + ",server_id" + serviceInstance.getServiceId());
+        return "Hello world";
+    }
+}
