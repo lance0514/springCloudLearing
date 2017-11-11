c129df499e75f602c240721e987db87b7d9f03d1
pengwao@163.com <pengwao@163.com>
2017-11-11 18:09:43 +0800

    提交代码

--- ribbon-consumer/src/main/java/com/ribbonconsumer/ribbonconsumer/ConsumerController.java
+++ ribbon-consumer/src/main/java/com/ribbonconsumer/ribbonconsumer/ConsumerController.java
@@ -1,16 +1,17 @@
 package com.ribbonconsumer.ribbonconsumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
-
+    @Autowired
     RestTemplate restTemplate;
 
-    @GetMapping("/ribbon-com")
-    public String helloConsumer(){
-        return restTemplate.getForEntity("http://hello-service/hello",String.class).getBody();
+    @GetMapping("/ribbon-consumer")
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
     }
 }
